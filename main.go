828b65c0b1e36a7f4196fc2b22aa607ced0a57cf
Philippe Carvalho <phdecarvalho@ebay.com>
2018-08-18 20:49:42 +0200

    Using TLS proxy approach

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import "crypto/tls"
-import "fmt"
-import "io"
-import "net"
-import "os"
-import "log"
-
-//import "strings"
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+)
 
 const serverKey = `-----BEGIN EC PARAMETERS-----
 BggqhkjOPQMBBw==
@@ -37,6 +35,9 @@ BbXaxBR98RACIQDNvEj2uojQfY2sV94kkJJxAAMXwejQpji6wjEEkOSqDg==
 -----END CERTIFICATE-----
 `
 
+const loAddr string = "127.0.0.1:10053"
+const reAddr string = "cloudflare-dns.com:853"
+
 func main() {
 	// Checking if the cert is valid
 	cert, err := tls.X509KeyPair([]byte(serverCert), []byte(serverKey))
@@ -45,7 +46,8 @@ func main() {
 	}
 	config := &tls.Config{Certificates: []tls.Certificate{cert}}
 
-	ln, err := net.Listen("tcp", ":10053")
+	//Opening Listener
+	ln, err := tls.Listen("tcp", loAddr, config)
 	if err != nil {
 		fmt.Println("Listeners Could not be opened")
 	}
@@ -63,11 +65,70 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		conn := tls.Server(connp, config)
-		go func(c net.Conn) {
-			io.Copy(os.Stdout, c)
-			fmt.Println()
-			c.Close()
-		}(conn)
+		go proxyFunc(connp)
+	}
+}
+
+func proxyFunc(conn net.Conn) {
+	defer conn.Close()
+
+	rAddr, err := net.ResolveTCPAddr("tcp", reAddr)
+	if err != nil {
+		fmt.Println("Cannot open connection to remote address")
+	}
+
+	reConn, err := net.DialTCP("tcp", nil, rAddr)
+	if err != nil {
+		log.Print(err)
+	}
+	defer reConn.Close()
+
+	Pipeline(conn, reConn)
+
+	fmt.Println("Connection ended with remote address")
+}
+
+func channCoon(conn net.Conn) chan []byte {
+	c := make(chan []byte)
+
+	go func() {
+		b := make([]byte, 1024)
+
+		for {
+			n, err := conn.Read(b)
+			if n > 0 {
+				res := make([]byte, n)
+				copy(res, b[:n])
+				c <- res
+			}
+			if err != nil {
+				c <- nil
+				break
+			}
+		}
+	}()
+
+	return c
+}
+
+func Pipeline(conn0 net.Conn, conn1 net.Conn) {
+	chan0 := channCoon(conn0)
+	chan1 := channCoon(conn1)
+
+	for {
+		select {
+		case b0 := <-chan0:
+			if b0 == nil {
+				return
+			} else {
+				conn1.Write(b0)
+			}
+		case b1 := <-chan1:
+			if b1 == nil {
+				return
+			} else {
+				conn0.Write(b1)
+			}
+		}
 	}
 }
